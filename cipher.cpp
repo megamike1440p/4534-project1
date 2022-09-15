30f228bbd0413b5850d3af2f67d8cfaca2c56330
megamike1440p <90876134+megamike1440p@users.noreply.github.com>
2022-09-15 11:06:14 -0500

    Update cipher.cpp

---
+++ cipher.cpp
@@ -0,0 +1,14 @@
+#include "cipher.hpp"
+
+Cipher(std::string passwordToEncrypt){
+}
+
+
+int getNumericKey(std::string key);
+
+
+void shuffleCipher(char cipherbet[26], char alphabet[26], std::string key);
+
+
+std::string encrypt(std::string passwordToEncrypt, char alphabet[26], char cipherbet[26]);
+
