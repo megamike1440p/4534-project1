45ffd96ed69f710b289b69d805a5735f02930fa6
megamike1440p <90876134+megamike1440p@users.noreply.github.com>
2022-09-15 20:02:54 -0500

    Update cipher.hpp

--- cipher.hpp
+++ cipher.hpp
@@ -19,6 +19,7 @@ class Cipher
 			void getNumericKey(std::string key);
 			void shuffleCipher(char cipherbet[26], char alphabet[26], std::string key);
 			std::string encrypt(std::string passwordToEncrypt, char alphabet[26], char cipherbet[26]);
+			char swapLetter(char letterToSwap, int passwordIndex, int alphabetIndex);
 	};
 
 #endif
