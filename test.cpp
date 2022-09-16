d52afb5141e6057c90a3d5d6f0394c558c89256c
megamike1440p <90876134+megamike1440p@users.noreply.github.com>
2022-09-16 04:48:06 -0500

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,3 +1,9 @@
+/***************************************************************
+  Student Name: Michael Whitten
+  File Name: Test.cpp
+  Assignment number: project #1
+This file containst the main method, which simply calls a single constructor. That constructor sets everything else in motion.
+***************************************************************/
 #include "cipher.hpp"
 #include "password-generator.hpp"
 
