aa3045de1a987848b454fa6df33354b269fee4b0
megamike1440p <90876134+megamike1440p@users.noreply.github.com>
2022-09-16 02:36:19 -0500

    Update cipher.cpp

--- cipher.cpp
+++ cipher.cpp
@@ -1,25 +1,28 @@
 #include "cipher.hpp"
 
-Cipher::Cipher(std::char passwordToEncrypt){
-	std::cout<<"default constructor called [cipher]" << std::endl;
+Cipher::Cipher(std::string passwordToEncrypt){
 	char alphabet[26] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
 	int numericKey[5] = {};
 	numericKeyPtr = numericKey;
 	alphabetPtr = alphabet;
 	this->key = "jones";
 	getNumericKey(key);
+	std::cout << "constructor complete, beginning encryption of chosen password: " << passwordToEncrypt << std::endl;
 	encrypt(passwordToEncrypt);
 }
 
 void Cipher::getNumericKey(std::string key){
+	std::cout << "numericKey = " << numericKeyPtr << std::endl;
 	for(int i = 0; i < 5; i++)
 	{
 		for (int j = 0; j <  26; j++)
 		{
 			if (key.at(i) == alphabetPtr[j])
 			{
+				std::cout << "alphabet index 0: " << alphabetPtr[0] << " alphabet index 25: " << alphabetPtr[25];
+				std::cout << "i = " << i << " j = " << j << " alphabetPtr[j] = " << alphabetPtr[j] << " key.at(i) = " << key.at(i);
 				numericKeyPtr[i] = j;
-				std::cout << numericKeyPtr[i] << std::endl;
+				std::cout << "numericKey: " << numericKeyPtr[i] << std::endl;
 			}
 		}
 	}
@@ -27,28 +30,30 @@ void Cipher::getNumericKey(std::string key){
 
 void Cipher::shuffleCipher(char cipherbet[26], char alphabet[26], std::string key)
 {
+	for (int i = 0; i < 5; i++){
+	
+	}
+
 }
 
-std::string Cipher::encrypt(std::string passwordToEncrypt){
-	std::cout << "encrypt called" << std::endl << ", password to encrypt: " << passwordToEncrypt << std::endl;
+void Cipher::encrypt(std::string passwordToEncrypt){
 
-	std::string encryptedPassword = "";
+	passwordEncrypted = "";
 	char letterToSwap;
 	for (int i = 0; i < 9; i++){
 		for (int j = 0; j < 26; j++){
 			if (passwordToEncrypt.at(i) == alphabetPtr[j]){
 				letterToSwap = alphabetPtr[j];
-				encryptedPassword += swapLetter(letterToSwap, i, j);
-
-				std::cout << encryptedPassword << std::endl;
+				std::cout << "current letter to swap: " << letterToSwap << std::endl;
+				passwordEncrypted += swapLetter(letterToSwap, i, j);
 			}
 		}
 	}
-	std::cout << "encrypted password = " << encryptedPassword;
-	return encryptedPassword;
+	std::cout << passwordEncrypted << std::endl;
 }
 
 char Cipher::swapLetter(char letterToSwap, int passwordIndex, int alphabetIndex){
+	std::cout << "begin swapLetter function" << std::endl;
 	int cipherIndex;
 
 	if (passwordIndex > 4)
@@ -56,22 +61,32 @@ char Cipher::swapLetter(char letterToSwap, int passwordIndex, int alphabetIndex)
 		passwordIndex = passwordIndex % 5;
 	}
 
+	//std::cout << "passwordIndex = " << passwordIndex << std::endl;
+
 	for (int i = 0; i < 26; i++){
+		//std::cout << "numericKeyPtr[passwordIndex] :" << numericKeyPtr[passwordIndex] << " alphabetPtr[i] : " << alphabetPtr[i] << std::endl;
 		if(key.at(passwordIndex) == alphabetPtr[i])
 		{
 			cipherIndex = i;
 			if(cipherIndex + alphabetIndex > 25)
 			{
+				//std::cout << "cipher index = " << cipherIndex << " alphabet index = " << alphabetIndex << std::endl;
 				letterToSwap = alphabetPtr[(cipherIndex+alphabetIndex)-26];
+				std::cout << "letter swapped with: " << letterToSwap << std::endl;
 				return letterToSwap;
 			}
 			
 			else
 			{
 				letterToSwap = alphabetPtr[cipherIndex+alphabetIndex];
+				std::cout << "letter swapped with: " << letterToSwap << std::endl;
 				return letterToSwap;
 			}
 		}
 	}
 	return letterToSwap;
+}
+
+std::string Cipher::getEncryptedPassword(){
+    return passwordEncrypted;
 }
